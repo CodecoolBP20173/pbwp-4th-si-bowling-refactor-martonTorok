1e795b33685f32b190fcb811b53bdfa7ce741c19
Márton Török <tormartonz@gmail.com>
2017-11-28 17:05:06 +0100

    2 lines merged into on (in line 34) // char.lower =='x' fixed // useless condition deleted, (== True) // in bowling.py

--- bowling.py
+++ bowling.py
@@ -7,12 +7,10 @@ def score(game):
             result += 10 - last
         else:
             result += get_value(game[i])
-        # if not in_first_half:
-            # frame += 1
         if frame < 10  and get_value(game[i]) == 10:
             if game[i] == '/':
                 result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
+            elif game[i].lower() == 'x':
                 result += get_value(game[i+1])
                 if game[i+2] == '/':
                     result += 10 - get_value(game[i+1])
@@ -21,22 +19,20 @@ def score(game):
         last = get_value(game[i])
         if not in_first_half:
             frame += 1
-        if in_first_half == True:
-            in_first_half = False
+        if in_first_half:
+            in_first_half = not in_first_half
         else:
             in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
+        if game[i].lower() == 'x':
             in_first_half = True
             frame += 1
     return result
 
 
 def get_value(char):
-    if int(char) > 1 and int(char) < 10:
+    if char in '123456789':
         return int(char)
-    elif char.lower() == 'x':
-        return 10
-    elif char == '/':
+    elif char.lower() in 'x/':
         return 10
     elif char == '-':
         return 0
