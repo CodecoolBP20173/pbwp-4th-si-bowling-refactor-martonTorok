3392c43928c02c78d86bafc19e0be7c065cd1ab0
Márton Török <tormartonz@gmail.com>
2017-11-28 12:03:00 +0100

    get_value if condition shortened,cleaned in bowling.py

--- bowling.py
+++ bowling.py
@@ -30,12 +30,11 @@ def score(game):
             frame += 1
     return result
 
+
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
+    if int(char) > 1 and int(char) < 10:
         return int(char)
-    elif char == 'X' or char == 'x':
+    elif char.lower() == 'x':
         return 10
     elif char == '/':
         return 10
